b15d6118b11aa21550a755cbc8072ac9707f5dc8
mihir7121 <mihir.nikam1@gmail.com>
2021-08-08 03:34:40 +0530

    Initial commit

---
+++ DetectionApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path("", views.landing, name='landing'),
+    path("upload", views.upload, name='upload'),
+    path("aboutus", views.aboutus, name='aboutus')
+    # path("index", views.index, name='index'),
+    # path("rendervideo", views.rendervideo, name='rendervideo'),
+    # path("takephoto", views.takephoto, name='takephoto'),
+    # path("about", views.aboutus, name='aboutus')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
---
+++ DetectionApp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from .forms import ImageForm
+import numpy as np
+import pandas as pd
+import keras
+from  keras.preprocessing.image import *
+import tensorflow as tf
+import os
+
+from PIL import Image as PImage
+# Create your views here.
+
+def landing(request):
+    return render(request, "DetectionApp/landing.html")
+
+def upload(request):
+    if(request.method=="POST"):
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+            image = img_obj.image.url
+            img_title = img_obj.title
+            a = predict_class(os.path.join("D:/Hackathons/XHacks/brainTumor/DetectionApp/"+img_title))
+            return render(request, 'DetectionApp/upload.html', {'form': form, 'img_obj':img_obj, 'image': image, 'title':img_title, 'BT':a})
+        else:
+            form = ImageForm()
+        return render(request, 'DetectionApp/upload.html', {'form': form})
+    else:
+        return render(request, "DetectionApp/upload.html")
+
+def aboutus(request):
+    return render(request, "DetectionApp/about.html")
+
+model = keras.models.load_model(os.path.join("D:/Hackathons/XHacks/brainTumor/DetectionApp/"+"model_optimal.h5")) #loading our model that we will use to make predictions of emotions
+def predict_class(img_path):
+    img = load_img(img_path)
+    image = img
+    newsize = (224, 224)
+    image = image.resize(newsize)
+    img_array_ = np.array(image)
+    img_array_ = np.expand_dims(img_array_, axis=0)
+    print(img_array_.shape)
+    prediction = (model.predict([img_array_])[0])
+    print(prediction)
+    if(prediction >= 0.5):
+        return 1
+    else:
+        return 0
